7999c2e2b35a06ab738f6dc4a0e27bf5f1ecbe4e
Aseem Gupta <500077426@stu.upes.ac.in>
2023-05-30 19:56:29 +0530

    Updated

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./Tu-meri-shab.mp3";
+import music2 from "./Teri-Yadon-mai.mp3";
+import music3 from "./Juda-hoke-bhi.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
